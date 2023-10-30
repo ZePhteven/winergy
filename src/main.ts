e8d2939af0456b32e5396c8f4981cd4e01d8857b
Steven <124669197+stevenlollivierpoc@users.noreply.github.com>
2023-10-30 21:32:50 +0100

    feat: add Swagger (w/ pnpm)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { version } from './../package.json';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
+
   await app.listen(3000);
 }
+
+function setupSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle('WinErgy API')
+    .setDescription('WinErgy API')
+    .setVersion(version);
+
+  const doc = options.build();
+
+  const document = SwaggerModule.createDocument(app, doc);
+  SwaggerModule.setup('', app, document);
+}
+
 bootstrap();
